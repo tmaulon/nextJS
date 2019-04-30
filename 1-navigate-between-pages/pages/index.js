88044cc49b23e448fcc0ceb46d96a2539446ebab
Thomas Maulon <tmaulon@betomorrow.com>
2019-04-30 13:09:28 +0200

    Basics - 1-navigate-between-pages

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,5 +1,14 @@
+// This is the Link API
+import Link from "next/link";
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a title="About Page">About Page</a>
+    </Link>
+    {/* <Link href="/about">
+      <button>About Page</button>
+    </Link> */}
     <p>Hello Next.js</p>
   </div>
 );
